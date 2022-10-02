219e782f8db4a71ba2ded969aaa225b1bab35376
dragonyc1002 <mivck1012@gmail.com>
2022-10-02 15:23:37 +0800

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './app.js';
-import './style.css';
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
